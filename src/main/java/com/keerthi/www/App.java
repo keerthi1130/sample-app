383613bfa6cf92fd300f0d31d2eaa44026272ac3
Cloud User <centos@ip-172-31-18-40.ap-south-1.compute.internal>
2019-02-01 11:07:53 +0000

    first commit

---
+++ src/main/java/com/keerthi/www/App.java
@@ -0,0 +1,13 @@
+package com.keerthi.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
